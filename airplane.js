d7858ae25115dda08f980d6482ccae5543936784
Astghik Simonyan <astghiksimonyan56@gmail.com>
2023-03-30 23:49:27 +0400

    feat(airplane task): completed as I got it(hope it's right)

--- airplane.js
+++ airplane.js
@@ -3,3 +3,19 @@
 // Give airplanes the ability to .takeOff() and .land():
 // If a plane takes off, its isFlying property is set to true.
 // If a plane lands, its isFlying property is set to false
+
+const airplain = {
+    name: "Fifi",
+    isFlying:false,
+    
+    takeOff(){
+        return this.isFlying = true;
+    },
+
+    land(){
+        return this.isFlying = false;
+    }
+}
+
+console.log(airplain.takeOff());
+console.log(airplain.land());
